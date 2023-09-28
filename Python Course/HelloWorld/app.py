9971c6e5b052e54c5e92375021b9439d5bd07dae
Omi Masika <omimasika@gmail.com>
2023-09-28 16:36:18 -0400

    exercise and solution

--- Python Course/HelloWorld/app.py
+++ Python Course/HelloWorld/app.py
@@ -1,8 +1,24 @@
 """Module providing a function printing python version."""
 
 
-def increment(number, by):
-    return (number, number + by)
+def fizz_buzz(input):
+    if (input % 3 == 0) and (input % 5 == 0):
+        return "FizzBuzz"
+    if input % 3 == 0:
+        return "Fizz"
+    if input % 5 == 0:
+        return "Buzz"
+    return input
 
 
-print(increment(2, 3))
+# def fizz_buzz(input):
+#     for input in fizz_buzz:
+#         if input % 3 == 0:
+#             print("Fizz")
+#         elif input % 5 == 0:
+#             print("Buzz")
+#         else:
+#             print(input)
+
+
+print(fizz_buzz(7))
